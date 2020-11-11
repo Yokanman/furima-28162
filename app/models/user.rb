2cb88aa9eaaf0783d2140803ae32a728d1f6656f
Yokanman <puresoulhatto@gmail.com>
2020-11-11 22:22:13 +0900

    バリデーション_本人情報確認

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,18 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :email, presence: true, inclusion: { in: ["@"], message: "is invalid. email needs to include @."}
+  validates :email, presence: true, inclusion: { in: ["@"], message: 'is invalid. email needs to include @.'}
   validates :password, presence: true,
-                       length: { minimum: 6, message: "is invalid Input more than 6 charactors." } 
-                       format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is invalid Input both half-width characters and numbers"}
+                       length: { minimum: 6, message: 'is invalid Input more than 6 charactors.' } 
+                       format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'is invalid Input both half-width characters and numbers' }
+  validates :first_name, presence: true
+             format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' }
+  validates :family_name, presence: true
+             format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' }
+  validates :first_name_kana, presence: true
+             format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナを使用してください' }
+  validates :family_name_kana, presence: true
+             format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナを使用してください' }
+  validates :birth_day, presence: true
 
 end
