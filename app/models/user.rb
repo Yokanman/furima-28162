9763bfe87a8a97630a54bfbaa663339eb3c398d5
Yokanman <puresoulhatto@gmail.com>
2020-11-11 21:57:45 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email, presence: true, inclusion: { in: ["@"], message: "is invalid. email needs to include @."}
+  validates :password, presence: true,
+                       length: { minimum: 6, message: "is invalid Input more than 6 charactors." } 
+                       format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is invalid Input both half-width characters and numbers"}
+
 end
