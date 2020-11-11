76d3c363fea8c989221f9e14cddcac7f858e4d35
Yokanman <puresoulhatto@gmail.com>
2020-11-11 22:49:06 +0900

    バリデーション_本人情報確認_2

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,15 +7,15 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :email, presence: true, inclusion: { in: ["@"], message: 'is invalid. email needs to include @.'}
   validates :password, presence: true,
-                       length: { minimum: 6, message: 'is invalid Input more than 6 charactors.' } 
+                       length: { minimum: 6, message: 'is invalid Input more than 6 charactors.' } ,
                        format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'is invalid Input both half-width characters and numbers' }
-  validates :first_name, presence: true
+  validates :first_name, presence: true,
              format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' }
-  validates :family_name, presence: true
+  validates :family_name, presence: true,
              format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' }
-  validates :first_name_kana, presence: true
+  validates :first_name_kana, presence: true,
              format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナを使用してください' }
-  validates :family_name_kana, presence: true
+  validates :family_name_kana, presence: true,
              format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナを使用してください' }
   validates :birth_day, presence: true
 
