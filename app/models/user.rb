4103f8b5e3706f7456d8a1ca70b60eb3cd00d2a7
Yokanman <puresoulhatto@gmail.com>
2020-11-14 00:51:00 +0900

    ログイン、ログアウト機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :email, presence: true, inclusion: { in: ["@"], message: 'is invalid. email needs to include @.'}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
   validates :password, presence: true,
                        length: { minimum: 6, message: 'is invalid Input more than 6 charactors.' } ,
                        format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'is invalid Input both half-width characters and numbers' }
