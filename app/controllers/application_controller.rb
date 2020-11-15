66a287436c615fd7a149fa67cf6d1929fb777d75
Yokanman <puresoulhatto@gmail.com>
2020-11-15 18:00:05 +0900

    バリデーション修正&案内コメント削除_1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :encrypted_password, :first_name, :family_name, :first_name_kana, :family_name_kana, :birth_day])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :family_name, :first_name_kana, :family_name_kana, :birth_day])
   end
 
   def basic_auth
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,19 +4,23 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
-  validates :password, presence: true,
-                       length: { minimum: 6, message: 'is invalid Input more than 6 charactors.' },
-                       format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'is invalid Input both half-width characters and numbers' }
-  validates :first_name, presence: true,
-                         format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字で入力してください' }
-  validates :family_name, presence: true,
-                          format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字で入力してください' }
-  validates :first_name_kana, presence: true,
-                              format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナで入力してください' }
-  validates :family_name_kana, presence: true,
-                               format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナで入力してください' }
-  validates :birth_day, presence: true
+  with_options presence: true do
+    validates :nickname
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+    validates :email, format: { with: VALID_EMAIL_REGEX }
+    validates :password,
+               length: { minimum: 6, message: 'is invalid Input more than 6 charactors.' },
+               format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'is invalid Input both half-width characters and numbers' }
+    VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/
+    validates :first_name,
+               format: { with: VALID_NAME_REGEX, message: '全角文字で入力してください' }
+    validates :family_name,
+               format: { with: VALID_NAME_REGEX, message: '全角文字で入力してください' }
+    VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/
+    validates :first_name_kana,
+               format: { with: VALID_NAME_KANA_REGEX, message: '全角カタカナで入力してください' }
+    validates :family_name_kana,
+               format: { with: VALID_NAME_KANA_REGEX, message: '全角カタカナで入力してください' }
+    validates :birth_day
+  end
 end
