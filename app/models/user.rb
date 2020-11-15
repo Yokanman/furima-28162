fde5e5a2dfbda1e443eecf886c4b1614a02f662a
Yokanman <puresoulhatto@gmail.com>
2020-11-15 18:20:52 +0900

    Rubocopの自動修正を実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,18 +9,18 @@ class User < ApplicationRecord
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
     validates :email, format: { with: VALID_EMAIL_REGEX }
     validates :password,
-               length: { minimum: 6, message: 'is invalid Input more than 6 charactors.' },
-               format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'is invalid Input both half-width characters and numbers' }
-    VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/
+              length: { minimum: 6, message: 'is invalid Input more than 6 charactors.' },
+              format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'is invalid Input both half-width characters and numbers' }
+    VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
     validates :first_name,
-               format: { with: VALID_NAME_REGEX, message: '全角文字で入力してください' }
+              format: { with: VALID_NAME_REGEX, message: '全角文字で入力してください' }
     validates :family_name,
-               format: { with: VALID_NAME_REGEX, message: '全角文字で入力してください' }
-    VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/
+              format: { with: VALID_NAME_REGEX, message: '全角文字で入力してください' }
+    VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
     validates :first_name_kana,
-               format: { with: VALID_NAME_KANA_REGEX, message: '全角カタカナで入力してください' }
+              format: { with: VALID_NAME_KANA_REGEX, message: '全角カタカナで入力してください' }
     validates :family_name_kana,
-               format: { with: VALID_NAME_KANA_REGEX, message: '全角カタカナで入力してください' }
+              format: { with: VALID_NAME_KANA_REGEX, message: '全角カタカナで入力してください' }
     validates :birth_day
   end
 end
