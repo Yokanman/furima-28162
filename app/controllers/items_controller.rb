b8538cf6cb8ece951e70803d5622ebda4f8b1cb9
Yokanman <puresoulhatto@gmail.com>
2020-11-18 00:02:06 +0900

    ActiveStorageアソシエーション記入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,20 @@
 class ItemsController < ApplicationController
   def index
+    @item = Item.all
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    redirect_to root_path
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image)
   end
 
 end
