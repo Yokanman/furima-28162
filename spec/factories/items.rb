5b636731309f9bbd9a3e46d665083ab2f8d6f7ab
Yokanman <puresoulhatto@gmail.com>
2020-11-22 18:01:41 +0900

    activehash項目修正、テストコード作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
-    
+    # image                 { ActionDispatch::Http::UploadedFile:0x00007f901586b198 @tempfile=#<Tempfile:/var/folders/8l/kz6cq8ls77x_0y8pj997dyd00000gn/T/RackMultipart20201122-50882-1qtxsvi.jpg>, @original_filename="mouseover-zoomup-image.jpg"}
+    name                  { 'cup' }
+    description           { 'コップです' }
+    category_id              { '2' }
+    condition_id             { '2' }
+    shipping_charger_id      { '2' }
+    shipping_from_id         { '2' }
+    days_to_shipping_id      { '2' }
+    value                 { '300' }
+    user_id               { '1' }
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品の出品' do
+  #  it '画像が無いと出品ができない' do
+  #    @item.image = ''
+  #    @item.valid?
+  #    expect(@item.errors.full_messages).to include("image can't be blank")
+  #  end
+   it '商品名が空だと出品できない' do
+    @item.name = ''
+    @item.valid?
+    expect(@item.errors.full_messages).to include("name can't be blank")
+   end
+   it '商品の説明が空だと出品できない' do
+    @item.description = ''
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Description can't be blank")
+   end
+   it 'カテゴリーが選択されていないと出品できない' do
+    @item.category = '1'
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Description can't be blank")
+   end
+  end
 end
