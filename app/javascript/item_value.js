6dceefe829d1f0472f251b12cd727d54bc585da2
Yokanman <puresoulhatto@gmail.com>
2020-11-22 22:35:39 +0900

    販売手数料、販売利益の表示

--- app/javascript/item_value.js
+++ app/javascript/item_value.js
@@ -1,5 +1,11 @@
-function post (){
-const priceInput = document.getElementById("item-price")
-  console.log(priceInput);
+function item (){
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+      const inputValue = priceInput.value;
+      const addTaxDom = document.getElementById("add-tax-price");
+      addTaxDom.innerHTML = Math.floor(inputValue*0.1);
+      const addProfitDom = document.getElementById("profit");
+      addProfitDom.innerHTML = Math.ceil(inputValue-inputValue*0.1)
+  });
 }
-window.addEventListener('load', post);
+window.addEventListener('load', item);
