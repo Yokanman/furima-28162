dddd73256038c1533fa92523a8f623f2da79906d
Yokanman <puresoulhatto@gmail.com>
2020-11-24 21:08:49 +0900

    商品一覧機能実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_120257) do
+ActiveRecord::Schema.define(version: 2020_11_24_105137) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -66,6 +66,17 @@ ActiveRecord::Schema.define(version: 2020_11_16_120257) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "users_purchase_logs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "item_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_users_purchase_logs_on_item_id"
+    t.index ["user_id"], name: "index_users_purchase_logs_on_user_id"
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "items", "users"
+  add_foreign_key "users_purchase_logs", "items"
+  add_foreign_key "users_purchase_logs", "users"
 end
---
+++ spec/factories/users_purchase_logs.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :users_purchase_log do
+    
+  end
+end
