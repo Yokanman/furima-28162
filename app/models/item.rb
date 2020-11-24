9df99dade2babf8f9437285a8cc1b66d5d43d4d0
Yokanman <puresoulhatto@gmail.com>
2020-11-24 21:14:03 +0900

    rubocop自動修正実行

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,12 +9,12 @@ class Item < ApplicationRecord
   belongs_to :user
 
   with_options presence: true do
-  validates :image
-  validates :name
-  validates :description
-  validates :value,
-             format: { with: /\A\d+\d\z/i, message: '半角数字で入力して下さい' },
-             numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: '300円以上9,999,999円以下で入力して下さい' }
+    validates :image
+    validates :name
+    validates :description
+    validates :value,
+              format: { with: /\A\d+\d\z/i, message: '半角数字で入力して下さい' },
+              numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: '300円以上9,999,999円以下で入力して下さい' }
   end
 
   with_options numericality: { other_than: 1, message: '項目を選択して下さい' } do
