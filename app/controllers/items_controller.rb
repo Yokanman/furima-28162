a921304652890d3533a60aa60ea011940c0fcfd4
Yokanman <puresoulhatto@gmail.com>
2020-11-28 23:23:54 +0900

    出品者ではない時、editできない

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :update, :edit]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -23,6 +23,9 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    unless current_user.id == @item.user.id
+      redirect_to root_path
+    end
   end
 
   def update
