44911d6f6208f68b7633255e4ebf26366d5617b8
Yokanman <puresoulhatto@gmail.com>
2020-12-02 21:04:46 +0900

    削除に関するコントローラの条件分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,6 +36,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    redirect_to root_path unless current_user.id != @item.user.id
     @item.destroy
     redirect_to root_path
   end
