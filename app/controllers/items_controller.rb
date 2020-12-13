8b98040d0d7118487aa39d7d5d48054289159360
Yokanman <puresoulhatto@gmail.com>
2020-12-13 21:42:52 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,7 +38,6 @@ class ItemsController < ApplicationController
   def destroy
     redirect_to root_path unless current_user.id != @item.user.id
     @item.destroy
-    redirect_to root_path
   end
 
   private
